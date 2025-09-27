fe8fec37dfd1ba41ccbf1d1c54e43cf42bb2fc3d
MediumToe <jordygonz@outlook.com>
2025-09-27 13:51:14 -0400

    Update Contribution

--- main.js
+++ main.js
@@ -5,7 +5,12 @@ const path = require('path');
 if (process.env.NODE_ENV === 'development') {
     try {
         require('electron-reload')(__dirname, {
-            electron: require(require('electron')?.path || 'electron')
+            electron: require(require('electron')?.path || 'electron'),
+            // Watch main.js and index.html explicitly
+            files: [
+                path.join(__dirname, 'main.js'),
+                path.join(__dirname, 'index.html')
+            ]
         })
     } catch (err) {
         // ignore if electron-reload isn't installed
